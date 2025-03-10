00eaa25cbb9a72a7277609250666354779642990
adrimartin6 <adrianmartincalvet@gmail.com>
2025-03-10 16:58:23 +0100

    Index crud tecnico empezado

--- resources/views/crudTecnico/index.blade.php
+++ resources/views/crudTecnico/index.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hola
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Dashboard Técnico')
+
+@section('content')
+
+<h2> Incidencias recibidas </h2>
+
+@endsection
