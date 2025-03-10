bb4ce5827cd3554cfd5ba2e5b8cc48e3067d7c1c
adrimartin6 <adrianmartincalvet@gmail.com>
2025-03-10 17:51:18 +0100

    Se ven incidencias asignadas a tecnicos

--- app/Http/Controllers/TecnicoController.php
+++ app/Http/Controllers/TecnicoController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Incidencia;
+use App\Models\IncidenciaUsuario;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class TecnicoController extends Controller
 {
     public function index(){
-        return view('/crudTecnico.index');
+        // Cojo el usuario logueado con su id para mostrar la incidencia que se le ha asignado
+        $comentarios = IncidenciaUsuario::where('user_id', Auth::id())
+            ->with('incidencia')
+            ->orderBy('created_at', 'desc')
+            ->get();
+            
+        return view('/crudTecnico.index', compact('comentarios'));
     }
 }
